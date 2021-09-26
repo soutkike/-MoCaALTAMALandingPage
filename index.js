e26c570dd8e3f2a0995204a55b3d98fc8c3cd830
Enrique Castillo <enrique.castillo.itcm@gmail.com>
2021-09-26 14:05:13 -0500

    Navbar working

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const sectionsHTML = document.querySelectorAll("section");
 const sections = [...sectionsHTML];
 const navitems = [...navItemsHTML];
 
+const handleColors = (navItems, index) => {
+  for (let i = 0; i < navItems.length; i++) {
+    if (i == index) {
+      navitems[i].classList.add("text-green-500");
+      navitems[i].classList.add("border-b-4");
+      navitems[i].classList.add("border-green-500");
+    } else {
+      navitems[i].classList.remove("text-green-500");
+      navitems[i].classList.remove("border-b-4");
+      navitems[i].classList.remove("border-green-500");
+    }
+  }
+};
+
 const callback = (entries, observer) => {
   entries.forEach((entrie, index) => {
     if (entrie.isIntersecting) {
       const i = sections.indexOf(entrie.target);
-      console.log(navitems[i]);
-      navitems[i].classList.add("text-green-500");
+      handleColors(navitems, i);
     }
   });
 };
